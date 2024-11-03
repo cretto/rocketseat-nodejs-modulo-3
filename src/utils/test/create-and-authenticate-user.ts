5b359c34cba46fc5a7418297a1ca3ec893b6eb91
cretto <cretto@users.noreply.github.com>
2024-11-03 17:06:30 -0300

    Modulo 7

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,12 +1,22 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'Henrique',
-    email: 'henrique@email.com',
-    password: '123456',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'Henrique',
+      email: 'henrique@email.com',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
+
+  await request(app.server).post('/users').send({})
 
   const authResponse = await request(app.server).post('/sessions').send({
     email: 'henrique@email.com',
